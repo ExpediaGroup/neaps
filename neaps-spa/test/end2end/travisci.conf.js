0ff6abbd3c4ee8bd2d0bb98fe778ee80992113ec
Giuseppe Sorrentino <uhpnou@gmail.com>
2018-01-19 13:02:06 +0100

    End2end tests on travisci through sauce-connect

---
+++ neaps-spa/test/end2end/travisci.conf.js
@@ -0,0 +1,68 @@
+/*
+
+Copyright 2018 Expedia Inc.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+require('babel-register')
+
+// http://nightwatchjs.org/guide#settings-file
+module.exports = {
+  "src_folders": ["test/end2end/specs"],
+  "output_folder": "test/end2end/reports",
+  "custom_assertions_path": ["test/end2end/custom-assertions"],
+
+  "selenium": {
+    "start_process": false,
+    "server_path": "node_modules/selenium-server/lib/runner/selenium-server-standalone-3.8.1.jar",
+    "host": "127.0.0.1",
+    "port": 4444,
+    "cli_args": {
+      "webdriver.chrome.driver": require('chromedriver').path
+    }
+  },
+
+  "test_settings": {
+    "default": {
+      "launch_url": "https://localhost:4445",
+      "selenium_port": 4445,
+      "selenium_host": "localhost",
+      "silent": true,
+      "username": process.env.SAUCE_USERNAME,
+      "access_key": process.env.SAUCE_ACCESS_KEY,
+      "screenshots": {
+        "enabled": false,
+        "path": "",
+      },
+      "globals": {
+        "waitForConditionTimeout": 10000,
+  },
+    },
+
+    "chrome": {
+      "desiredCapabilities": {
+        "browserName": "chrome",
+        "platform": "Windows 10",
+        "version": "63"
+      }
+    },
+
+    "ie11": {
+      "desiredCapabilities": {
+        "browserName": "internet explorer",
+        "platform": "Windows 10",
+        "version": "11.0"
+      }
+    }
+  }
+}
